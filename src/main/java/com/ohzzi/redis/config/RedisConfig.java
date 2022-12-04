45c62567dfa36dd6eff2b1d98322e4db8d099331
Ohzzi <jihoon970712@gmail.com>
2022-12-04 16:21:29 +0900

    chore: EmbeddedRedis 세팅

---
+++ src/main/java/com/ohzzi/redis/config/RedisConfig.java
@@ -0,0 +1,25 @@
+package com.ohzzi.redis.config;
+
+import org.redisson.spring.data.connection.RedissonConnectionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new RedissonConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
+    }
+}
