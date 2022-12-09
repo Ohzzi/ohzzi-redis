0e6bed8a1d6ea74b1c9cd2ab38905ac2f7d053dc
Ohzzi <jihoon970712@gmail.com>
2022-12-09 18:10:01 +0900

    test: 동시성 테스트 클래스 명 변경

--- src/test/java/com/ohzzi/redis/concurrency/ConcurrencyTest.java
+++ src/test/java/com/ohzzi/redis/concurrency/ConcurrencyTest.java
@@ -11,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BookServiceTest {
+class ConcurrencyTest {
 
     @Autowired
     private BookRepository bookRepository;
